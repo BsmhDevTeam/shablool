17595e4942466d2ded21596871f13b960eaacd1b
BsmhDev <bsmhdevteam@gmail.com>
2017-04-23 11:19:55 +0300

    - update meteor - add manage and game layout - add edit-quiz page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,8 +1,12 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// Import needed templates
+// Import layouts
 import '../../ui/layouts/body/body.js';
+import '../../ui/layouts/game/game.js';
+import '../../ui/layouts/manage/manage.js';
+
+// Import pages
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
 import '../../ui/pages/createQuiz/createQuiz.js';
