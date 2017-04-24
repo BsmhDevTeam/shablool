f7ee21901c7b19fec861d42c53557ae9cf8884f0
BsmhDev <bsmhdevteam@gmail.com>
2017-04-24 12:13:48 +0300

    - add reactive-dict library - add uuid library - impl add question functionality - impl remove functionality

--- imports/ui/pages/create-quiz/create-quiz.js
+++ imports/ui/pages/create-quiz/create-quiz.js
@@ -1,16 +1,39 @@
 import './create-quiz.html';
 import '../../components/question-form/question-form.js';
+import uuidV4 from 'uuid/v4';
+
+
+Template.create_quiz.onCreated(function () {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    questions: [uuidV4()],
+  });
+});
+
+Template.create_quiz.helpers({
+  questions() {
+    const instance = Template.instance();
+    return instance.state.get('questions');
+  },
+  removeQuestion(id) {
+    const state = Template.instance().state;
+    return () => () => state.set('questions', state.get('questions').filter(i => i!==id));
+  }
+});
 
 Template.create_quiz.events({
-  'click .save-quiz'(event, instance) {
-    event.preventDefault();
-    forms = instance.$('.question-form');
-    
-    objs = forms.map((i, form) => {
-        return {
-            q: form.question.value,
-            as: [form.answer1.value, form.answer2.value, form.answer3.value, form.answer4.value]
-        }
+  'click .add-question' (event, instance) {
+    const questions = instance.state.get('questions');
+    instance.state.set('questions', [...questions, uuidV4()]);
+  },
+  'click .save-quiz' (event, instance) {
+    const forms = instance.$('.question-form');
+
+    const objs = forms.map((i, form) => {
+      return {
+        q: form.question.value,
+        as: [form.answer1.value, form.answer2.value, form.answer3.value, form.answer4.value]
+      }
     });
   },
 });
