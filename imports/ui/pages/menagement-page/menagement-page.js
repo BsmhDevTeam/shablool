abb924bf3af79694d0d61b2df694c9b82b6e45b9
Gal Sela <Gal762@gmail.com>
2017-05-18 15:01:49 +0300

    Edit quiz - answer + Delete

--- imports/ui/pages/menagement-page/menagement-page.js
+++ imports/ui/pages/menagement-page/menagement-page.js
@@ -1,15 +1,29 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './menagement-page.html';
+import Quiz from '../../../api/quizes/quizes';
 
-import Quizes from '../../../api/quizes/quizes';
-
-Template.menagementPage.onCreated( function() {
+Template.menagementPage.onCreated(function() {
   Template.instance().subscribe('quiz-title');
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    quizes: Quiz.find(),
+  });
 });
-
-//const names = ["תכנות בסיסי","תכנות מתקדם חרדים", "סייבר סייבר סייביר"];
 
 Template.menagementPage.helpers({
   getQuizes() {
-  	return Quizes.find();
- }
+    return Quiz.find();
+  },
+
+  deleteQuiz(quiz) {
+    Quiz.remove(quiz);
+  },
+});
+
+Template.menagementPage.events({
+  'click .delete': function() {
+    return this.delete();
+  },
 });
