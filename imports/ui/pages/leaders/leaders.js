fccba0aa1c9cbb11769d7674fce2301914debc8a
שגב להב <segevlahav0@gmail.com>
2017-06-22 01:15:45 +0300

    fix leaders screen

--- imports/ui/pages/leaders/leaders.js
+++ imports/ui/pages/leaders/leaders.js
@@ -12,32 +12,36 @@ const Leaders = ({ game }) => {
     <div id="leaders">
       <div className="game-background" />
       {game.quiz.owner === Meteor.userId()
-              ? <a
-                href="javacript:void(0)"
-                className="btn btn-primary"
-                onClick={nextQuestion}
-              >
-                  לשאלה הבאה
-                </a>
+              ? <div className="row">
+                  <div className="next-question-button">
+                    <a
+                    href="javacript:void(0)"
+                    className="btn btn-primary"
+                    onClick={nextQuestion}
+                    >
+                      לשאלה הבאה
+                    </a>
+                  </div>
+                </div>
               : ''}
       <div className="row">
         <GameNavbar text="Leaders" num="" />
       </div>
       <div className="row">
-        <div className="table">
-          <tbody className="table-style">
+        <table className="table leaders-table">
+          <tbody>
             {game.scoreList().map((object, index) => (
               <tr className={index === 0 ? 'leader' : ''}>
-                <td>
-                  {object.userName}
-                </td>
                 <td className="to-the-right">
                   {object.userScore}
+                </td>
+                <td>
+                  {object.userName}
                 </td>
               </tr>
             ))}
           </tbody>
-        </div>
+        </table>
       </div>
     </div>
   );
