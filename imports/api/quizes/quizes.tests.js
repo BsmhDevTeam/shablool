b298e335969afaac0163bcf8e0977aa2b8664a5f
BsmhDev <bsmhdevteam@gmail.com>
2017-06-29 13:41:50 +0300

    fixind warrnings

---
+++ imports/api/quizes/quizes.tests.js
@@ -0,0 +1,10 @@
+// import { describe, it } from 'mocha';
+// import { expect } from 'chai';
+
+// const foo = 'foo';
+
+// describe('foo', function () {
+//   it('must be String', function() {
+//     expect(foo).to.be.a('string');
+//   });
+// });
---
+++ imports/ui/pages/game-shared/game-close.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+const GameClose = () => {
+  const backToHome = () => {
+    FlowRouter.go('Game.Home');
+  };
+  return (
+    <div id="game-close">
+      <div className="game-background" />
+      <div>
+        <a href="javascript:void(0)" className="btn btn-primary show-leaders-btn" onClick={backToHome}>
+            לטבלת המובילים
+        </a>
+      </div>
+      <div className="row">
+        <div id="title">
+          <h1>המשחק הסתיים</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameClose;
