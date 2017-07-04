66e49e249f0a70a83041cc7a106f47cf40ac0e02
lidor cohen <lidorcg@gmail.com>
2017-07-04 15:27:06 +0300

    - add readme - add example settings - add example docker-compose.yml

--- imports/startup/server/authentication.js
+++ imports/startup/server/authentication.js
@@ -1,25 +1,10 @@
+import { Meteor } from 'meteor/meteor';
 import { ServiceConfiguration } from 'meteor/service-configuration';
 
-ServiceConfiguration.configurations.upsert(
-  { service: 'github' },
-  {
-    $set: {
-      clientId: '09eb4c188eadb9503077',
-      loginStyle: 'popup',
-      secret: '5581151c7e0942bf1b51faa555de3a354d2c7969',
-    },
-  },
-);
 
 ServiceConfiguration.configurations.upsert(
   { service: 'gitlab' },
   {
-    $set: {
-      clientId: '3972cd31c474a8cac37c4c75f7cf4592f7014998aab662ee0fe111b674883351',
-      loginStyle: 'popup',
-      secret: '40726a4ac7a012b52c75e2a062119b6143a9cfdc8461c115680c7d7fbac0a88a',
-    },
+    $set: Meteor.settings.auth.gitlab,
   },
 );
-
-// TODO: PRODUCTION - configure service clientId and secret
