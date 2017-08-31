03a5e4bc2fabd1a09b9805fbf9cd0c615100caac
Segev95 <segevlahav0@gmail.com>
2017-08-31 10:29:48 +0300

    adding publications

--- imports/api/images/server/publication.js
+++ imports/api/images/server/publication.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import Image from '../images.js';
 
 Meteor.publish('images.all', function() {
   return Image.find().cursor;
+});
+
+Meteor.publish('images.by-id', function(id) {
+  check(id, String);
+  return Image.find({ _id: id });
 });
